dca7654e7a3c6d9e7dadcace48263e30008e9941
James Halliday <mail@substack.net>
2011-10-14 19:42:33 -0700

    better post test not using silly shim modules, passes

--- test/post.js
+++ test/post.js
@@ -1,39 +1,36 @@
 var test = require('tap').test;
 var bouncy = require('bouncy');
 var http = require('http');
-var Stream = require('./lib/stream');
+var net = require('net');
 
 test('POST with http', function (t) {
     var port = Math.floor(Math.random() * (Math.pow(2,16) - 1e4) + 1e4);
-    t.plan(4);
-    var s = bouncy(function (req, bounce) {
+    t.plan(3);
+    var b = bouncy(function (req, bounce) {
         t.equal(req.headers.host, 'localhost:' + port);
-        
-        var stream = Stream();
-        var data = '';
-        var alive = true;
-        
-        stream.on('data', function (buf) {
-            data += buf.toString();
-            if (alive && data.match(/pow!/)) {
-                t.ok(true, 'got post data');
-                stream.end();
-                alive = false;
-            }
-        });
-        
-        bounce(stream);
-        
-        stream.write([
-            'HTTP/1.1 200 200 OK',
-            'Content-Type: text/plain',
-            'Connection: close',
-            '',
-            'oh hello'
-        ].join('\r\n'));
+        bounce(p);
     });
     
-    s.listen(port, function () {
+    var p = Math.floor(Math.random() * (Math.pow(2,16) - 1e4) + 1e4);
+    var s = net.createServer(function (c) {
+        var data = '';
+        c.on('data', function (buf) {
+            data += buf.toString();
+            if (data.match(/\r\n\r\n$/)) {
+                c.write([
+                    'HTTP/1.1 200 200 OK',
+                    'Content-Type: text/plain',
+                    'Connection: close',
+                    '',
+                    'oh hello'
+                ].join('\r\n'));
+                c.end();
+            }
+        });
+    });
+    s.listen(p);
+    
+    b.listen(port, function () {
         var opts = {
             method : 'POST',
             host : 'localhost',
@@ -49,6 +46,7 @@ test('POST with http', function (t) {
                 if (data === 'oh hello') {
                     t.equal(data, 'oh hello');
                     res.socket.end();
+                    b.close();
                     s.close();
                     t.end();
                 }
